ac14fb2f4e13e50f8fe490f2bf3504eec7111df9
natalia <nataliasolcasanovas@gmail.com>
2017-12-17 00:10:11 -0300

    Agrego vistas RandomSongs

---
+++ WebApplication2/Controllers/RandomSongController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using System.Data.SqlClient;
+
+namespace WebApplication2.Controllers
+{
+    public class RandomSongController : Controller
+    {
+        private MusicMovieContext db = new MusicMovieContext();
+
+        // GET: RandomMovie
+        public ActionResult RandomHappy()
+        {
+
+
+
+
+            return View(db.Songs.OrderBy(c => Guid.NewGuid()).FirstOrDefault(m => m.Mood_ID == 1));
+
+
+
+        }
+        public ActionResult RandomPassionate()
+        {
+
+
+
+
+            return View(db.Songs.OrderBy(c => Guid.NewGuid()).FirstOrDefault(m => m.Mood_ID == 2));
+
+
+        }
+        public ActionResult RandomMelancholic()
+        {
+
+
+
+
+            return View(db.Songs.OrderBy(c => Guid.NewGuid()).FirstOrDefault(m => m.Mood_ID == 3));
+
+
+        }
+        public ActionResult RandomPeaceful()
+        {
+
+
+
+            return View(db.Songs.OrderBy(c => Guid.NewGuid()).FirstOrDefault(m => m.Mood_ID == 4));
+
+
+        }
+    }
+}
