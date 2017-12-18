e8e3ad82999393b4053c83d94cff2108e900cb6e
natalia <nataliasolcasanovas@gmail.com>
2017-12-17 22:21:07 -0300

    Agrego historial

---
+++ WebApplication2/Controllers/HistorialMoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using System.Data.SqlClient;
+
+
+namespace WebApplication2.Controllers
+{
+    public class HistorialMoviesController : Controller
+    {
+        private MusicMovieContext db = new MusicMovieContext();
+        
+        // GET: HistorialMovies
+        public ActionResult HistorialMovies()
+            
+        {
+            var historialMovies = db.Movies.OrderBy(c => Guid.NewGuid()).ToList();
+            
+
+
+            return View(historialMovies.ToList());
+        }
+    }
+}
